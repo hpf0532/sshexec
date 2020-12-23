b38f27c25bed6e3d70fcb66d80142ab7603d8f78
hpf <hpf1992@126.com>
2020-12-23 17:53:55 +0800

    update with go mod

--- example/main.go
+++ example/main.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-	"time"
-	"github.com/gaoyue1989/sshexec"
+	"github.com/hpf0532/sshexec"
 	"log"
+	"time"
 )
 
 func main() {
 	sshExecAgent := sshexec.SSHExecAgent{}
-	sshExecAgent.Worker = 10;
+	sshExecAgent.Worker = 10
 	sshExecAgent.TimeOut = time.Duration(120) * time.Second
-	s, err := sshExecAgent.SftpHostByKey([]string{"193.168.2.1","193.168.2.2"}, 22,"root", "/data/test.log", "/data/test.log")
-	log.Println("res:",s)
-	log.Println("err:",err)
+	s, err := sshExecAgent.SftpHostByKey([]string{"193.168.2.1", "193.168.2.2"}, 22, "root", "/data/test.log", "/data/test.log")
+	log.Println("res:", s)
+	log.Println("err:", err)
 
-	s1, err1 := sshExecAgent.SshHostByKey([]string{"193.168.2.1","193.168.2.2"}, 22,"root", "ll")
-	log.Println("res:",s1)
-	log.Println("err:",err1)
+	s1, err1 := sshExecAgent.SshHostByKey([]string{"193.168.2.1", "193.168.2.2"}, 22, "root", "ll")
+	log.Println("res:", s1)
+	log.Println("err:", err1)
 	for {
 		time.Sleep(1000)
 	}
---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/hpf0532/gopub
+
+go 1.15
+
+require (
+	github.com/hpf0532/sshexec v0.0.0-20180411015241-03e3607309f3
+	github.com/linclin/grpool v0.0.0-20170608104027-1e728f61a9da
+	github.com/pkg/sftp v1.12.0
+	golang.org/x/crypto v0.0.0-20201217014255-9d1352758620
+)
+
+replace github.com/hpf0532/sshexec v0.0.0-20180411015241-03e3607309f3 => ./
